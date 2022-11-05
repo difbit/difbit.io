61cae0fbf3030e2dd2eee99010cb7e3cab69ba68
difbit <timo.kekalainen@hotmail.com>
2022-11-05 10:01:49 +0200

    test paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,11 +17,7 @@ export default function Home() {
   return (
     <div>
       <P>
-        This is an example single page app built with React and
-        React&nbsp;Router using <Code>BrowserRouter</Code>. Navigate with the
-        links below and refresh the page or copy/paste the url to test out the
-        redirect functionality deployed to overcome GitHub&nbsp;Pages
-        incompatibility with single page apps (like this one).
+        Test paragraph
       </P>
       <P>
         Please see the <RepoReadmeLink /> for instructions on how to use this

a203ed06f53b25a7e7d43b703819cd39dbc09466
difbit <timo.kekalainen@tawasta.fi>
2023-11-19 20:49:33 +0200

    reroute

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import React from 'react';
 //import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Home from './pages';
+import Home from './pages';
 import About from './pages/about';
 import Finnish from './pages/finnish';
 import Pictures from './pages/pictures';
 
-//<Route path='/' exact element={<Home />} />
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' exact element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/finnish' element={<Finnish />} />
         <Route path='/pictures' element={<Pictures />} />
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 
 import App from './App';
-import Reroute from './App';
+//import Reroute from './App';
 
 //import reportWebVitals from './reportWebVitals';
 //import registerServiceWorker from "./registerServiceWorker";

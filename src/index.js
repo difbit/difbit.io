91623c8209b217ad27a9198edc90b65b6f206d16
difbit <timo.kekalainen@tawasta.fi>
2023-11-25 16:53:56 +0200

    Reroute

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import "./App.css";
 import App from './App.js';
-//import Reroute from './Reroute.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

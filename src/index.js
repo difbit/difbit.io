91c54e2249644019bb7e6000f411e407faf51638
difbit <timo.kekalainen@tawasta.fi>
2023-11-25 15:43:58 +0200

    import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import "./App.css";
-import App from './App';
-//import Reroute from './App';
+import App from './App.js';
+import Reroute from './Reroute.js';
 
 //import reportWebVitals from './reportWebVitals';
 //import registerServiceWorker from "./registerServiceWorker";
